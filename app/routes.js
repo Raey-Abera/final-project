e9df2f079e5ce7a4e5c460c7cc6adcd45ccad765
Raey Abera <raeyabera@Raeys-MacBook-Pro.local>
2019-04-05 23:48:55 -0400

    first commit

--- app/routes.js
+++ app/routes.js
@@ -27,7 +27,7 @@ module.exports = function(app, passport, db) {
 // message board routes ===============================================================
 
     app.post('/messages', (req, res) => {
-      db.collection('messages').save({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
+      db.collection('messages').save({name: req.body.name, msg: req.body.msg}, (err, result) => {
         if (err) return console.log(err)
         console.log('saved to database')
         res.redirect('/profile')
@@ -37,9 +37,9 @@ module.exports = function(app, passport, db) {
     app.put('/messages', (req, res) => {
       db.collection('messages')
       .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
-        $set: {
-          thumbUp:req.body.thumbUp + 1
-        }
+        // $set: {
+        //   thumbUp:req.body.thumbUp + 1
+        // }
       }, {
         sort: {_id: -1},
         upsert: true
--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://demo:demo@cluster0-92dvh.mongodb.net/test?retryWrites=true', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    'dbName': 'demo'
+    'url' : 'mongodb+srv://final:final@cluster0-rapcn.mongodb.net/test?retryWrites=true', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'game'
 };
